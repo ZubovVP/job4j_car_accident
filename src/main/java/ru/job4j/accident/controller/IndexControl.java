1e7b199b8814f6265b8f39f93c149d8ba4d8f8ca
ZubovVP <Zubov.VP@yandex.ru>
2020-10-20 22:53:55 +0300

    Correct Accident.

--- src/main/java/ru/job4j/accident/controller/IndexControl.java
+++ src/main/java/ru/job4j/accident/controller/IndexControl.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.job4j.accident.model.Accident;
-import java.util.HashMap;
+import ru.job4j.accident.repository.AccidentMem;
+
 import java.util.Map;
+
 
 /**
  * Created by Intellij IDEA.
@@ -16,31 +18,12 @@ import java.util.Map;
  */
 @Controller
 public class IndexControl {
+    private static final AccidentMem ACCIDENT_MEM = new AccidentMem();
+
     @GetMapping("/")
     public String index(Model model) {
-        Map<Integer, Accident> map = new HashMap<>();
-        Accident accident = new Accident();
-        accident.setId(1);
-        accident.setName("name");
-        accident.setAddress("adress");
-        accident.setText("test");
-        map.put(accident.getId(), accident);
-
-        Accident accident2 = new Accident();
-        accident2.setId(2);
-        accident2.setName("name2");
-        accident2.setAddress("adress2");
-        accident2.setText("test2");
-        map.put(accident2.getId(), accident2);
-
-        Accident accident3 = new Accident();
-        accident3.setId(3);
-        accident3.setName("name3");
-        accident3.setAddress("adress3");
-        accident3.setText("test3");
-        map.put(accident3.getId(), accident3);
-
-        model.addAttribute("accidents", map);
+        Map<Integer, Accident> result = ACCIDENT_MEM.getAccidents();
+        model.addAttribute("accidents", result);
         return "index";
     }
 }
--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 
-import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Intellij IDEA.
@@ -21,5 +21,35 @@ import java.util.HashMap;
 @Data
 @EqualsAndHashCode
 public class AccidentMem {
-private HashMap<Integer, Accident> accidents;
+    private Map<Integer, Accident> accidents;
+
+    public Map<Integer, Accident> getAllAccidents() {
+        fillAccidents();
+        return this.accidents;
+    }
+
+    private void fillAccidents() {
+        if (this.accidents == null) {
+            Accident accident = new Accident();
+            accident.setId(1);
+            accident.setName("name");
+            accident.setAddress("adress");
+            accident.setText("test");
+            this.accidents.put(accident.getId(), accident);
+
+            Accident accident2 = new Accident();
+            accident2.setId(2);
+            accident2.setName("name2");
+            accident2.setAddress("adress2");
+            accident2.setText("test2");
+            this.accidents.put(accident2.getId(), accident2);
+
+            Accident accident3 = new Accident();
+            accident3.setId(3);
+            accident3.setName("name3");
+            accident3.setAddress("adress3");
+            accident3.setText("test3");
+            this.accidents.put(accident3.getId(), accident3);
+        }
+    }
 }
